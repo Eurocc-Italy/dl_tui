4b5558d71566c216fa2c0fa374226b6f27648bfe
Luca Babetto <l.babetto@cineca.it>
2023-10-16 15:42:30 +0200

    added filter function

--- tui/core/db.py
+++ tui/core/db.py
@@ -6,10 +6,12 @@ Functions and utilities to interface with the MongoDB database on the VM
 Author: @lbabetto
 """
 
-from typing import Callable, Any
-
 from pymongo import MongoClient
 from hpc import copy_files, launch_job
+
+from typing import Dict, Any
+from pymongo.database import Database
+from pymongo.collection import Collection
 
 import logging
 
@@ -24,15 +26,30 @@ MONGODB_URI = f"mongodb://{USER}:{PASSWORD}@{IP}:{PORT}/"
 
 
 def show_db_details(database: str):
+    """Show database details (collections, ...)
+
+    Parameters
+    ----------
+    database : str
+        name of the database
+    """
     client = MongoClient(MONGODB_URI)
     db = client[database]
     print(f"Database: {database}")
-    print(f"  collections:")
+    print(f"  - Collections:")
     for collection in db.list_collections():
         print(f"    - {collection['name']}")
 
 
-def run_query(database: str, collection: str, query: Callable):
+def filter(database: str, collection: str, filter_dict: Dict[str, Any]):
+    client = MongoClient(MONGODB_URI)
+    coll = client[database][collection]
+
+    for entry in coll.find(filter_dict):
+        return entry
+
+
+def run_query(database: str, collection: str, query):
     """Wraps the user query function and runs it on the database
 
     Parameters
