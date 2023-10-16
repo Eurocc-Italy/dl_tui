0fc757d4ede1a3211a747f8c5b2bab166fed6802
Luca Babetto <l.babetto@cineca.it>
2023-10-16 16:30:39 +0200

    updated slurm.sh

--- tui/core/slurm.sh
+++ tui/core/slurm.sh
@@ -4,7 +4,6 @@
 #SBATCH --time=00:01:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=375300MB
 module load python
 source $HOME/virtualenvs/dev/bin/activate
-./main.py > output.out 2> error.err
+python ./main.py > output.out 2> error.err
