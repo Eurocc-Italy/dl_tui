d47866c2ee202ff52b4e03839e66871d811c47b6
Luca Babetto <l.babetto@cineca.it>
2023-10-23 16:10:05 +0200

    temporarily changed resource quantity

--- tui/core/slurm.sh
+++ tui/core/slurm.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --partition=g100_usr_prod       
 #SBATCH --account=cin_staff
-#SBATCH --time=00:01:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=48
 module load python
 source $HOME/virtualenvs/dev/bin/activate
 python ./SCRIPT FILES > output.out 2> error.err
