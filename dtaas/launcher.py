d2035a7f1c23e50caef1930c20539922609d5b0f
Luca Babetto <l.babetto@cineca.it>
2023-10-31 14:52:04 +0100

    test

--- dtaas/launcher.py
+++ dtaas/launcher.py
@@ -41,16 +41,16 @@ source {config["HPC"]["venv_path"]}; \
 python {config["HPC"]["repo_dir"]}/wrapper.py --query """{query}""" --script """{script}"""'
 
     # bash commands to be run via ssh; TODO: decide structure of temporary folders
-    ssh_cmd = f"mkdir dtaas_tui_tests; \
+    ssh_cmd = f'mkdir dtaas_tui_tests; \
 mkdir dtaas_tui_tests/{os.path.basename(os.getcwd())}; \
 cd dtaas_tui_tests/{os.path.basename(os.getcwd())}; \
-sbatch -p {partition} -A {account} -t {walltime} -N {nodes} --ntasks-per-node 48 --wrap '''{wrap_cmd}'''"
+sbatch -p {partition} -A {account} -t {walltime} -N {nodes} --ntasks-per-node 48 --wrap "{wrap_cmd}"'
 
     logging.debug(f"Launching command via ssh: {ssh_cmd}")
 
     stdout, stderr = subprocess.Popen(
         # TODO: key currently necessary, will be removed when we switch to chain user
-        f"ssh -i /home/centos/.ssh/luca-hpc {config['HPC']['user']}@{config['HPC']['host']} '{ssh_cmd}'",
+        f"ssh -i /home/centos/.ssh/luca-hpc {config['HPC']['user']}@{config['HPC']['host']} \"{ssh_cmd}\"",
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
