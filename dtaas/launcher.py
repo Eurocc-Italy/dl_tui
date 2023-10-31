36a47858ccbe9dca64d0f2946b334a772363d4a5
Luca Babetto <l.babetto@cineca.it>
2023-10-31 15:12:44 +0100

    test

--- dtaas/launcher.py
+++ dtaas/launcher.py
@@ -14,7 +14,7 @@ config = utils.load_config()
 import logging
 
 logging.basicConfig(
-    # filename=config["LOGGING"]["logfile"],
+    filename=config["LOGGING"]["logfile"],
     format=config["LOGGING"]["format"],
     level=config["LOGGING"]["level"].upper(),
 )
