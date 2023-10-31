3ff7f1e9daf9d1a371e942ed895ed3442b701166
Luca Babetto <l.babetto@cineca.it>
2023-10-31 15:35:11 +0100

    test

--- dtaas/launcher.py
+++ dtaas/launcher.py
@@ -19,51 +19,57 @@ logging.basicConfig(
     level=config["LOGGING"]["level"].upper(),
 )
 
-"""Launch Slurm job on G100 with the user script on the files returned by the TUI filter
 
-Parameters
-----------
-query : str
-    SQL query to be passed to the wrapper on HPC
-script : str
-    Python script for the processing of the query results
-"""
+def launch_job(query, script):
+    """Launch Slurm job on G100 with the user script on the files returned by the TUI filter
 
-# Parsing API input, requires --query keyword, with optional --script
-parser = argparse.ArgumentParser()
-parser.add_argument("--query", type=str, required=True)
-parser.add_argument("--script", type=str, required=False)
-args = parser.parse_args()
-logging.debug(f"API input (launcher): {args}")
+    Parameters
+    ----------
+    query : str
+        SQL query to be passed to the wrapper on HPC
+    script : str
+        Python script for the processing of the query results
+    """
 
-# SLURM parameters
-partition = config["HPC"]["partition"]
-account = config["HPC"]["account"]
-walltime = config["HPC"]["walltime"]
-nodes = config["HPC"]["nodes"]
-wrap_cmd = f'module load python; \
+    # SLURM parameters
+    partition = config["HPC"]["partition"]
+    account = config["HPC"]["account"]
+    walltime = config["HPC"]["walltime"]
+    nodes = config["HPC"]["nodes"]
+    wrap_cmd = f'module load python; \
 source {config["HPC"]["venv_path"]}; \
-python {config["HPC"]["repo_dir"]}/wrapper.py --query """{args.query}""" --script """{args.script}"""'
+python {config["HPC"]["repo_dir"]}/wrapper.py --query """{query}""" --script """{script}"""'
 
-# bash commands to be run via ssh; TODO: decide structure of temporary folders
-ssh_cmd = f"mkdir dtaas_tui_tests; \
+    # bash commands to be run via ssh; TODO: decide structure of temporary folders
+    ssh_cmd = f"mkdir dtaas_tui_tests; \
 mkdir dtaas_tui_tests/{os.path.basename(os.getcwd())}; \
 scp vm:{os.getcwd()}/config.json .; \
 cd dtaas_tui_tests/{os.path.basename(os.getcwd())}; \
 sbatch -p {partition} -A {account} -t {walltime} -N {nodes} --ntasks-per-node 48 --wrap '{wrap_cmd}'"
 
-logging.debug(f"Launching command via ssh: {ssh_cmd}")
+    logging.debug(f"Launching command via ssh: {ssh_cmd}")
 
-stdout, stderr = subprocess.Popen(
-    # TODO: key currently necessary, will be removed when we switch to chain user
-    f'ssh -i /home/centos/.ssh/luca-hpc {config["HPC"]["user"]}@{config["HPC"]["host"]} "{ssh_cmd}"',
-    shell=True,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.PIPE,
-).communicate()
+    stdout, stderr = subprocess.Popen(
+        # TODO: key currently necessary, will be removed when we switch to chain user
+        f'ssh -i /home/centos/.ssh/luca-hpc {config["HPC"]["user"]}@{config["HPC"]["host"]} "{ssh_cmd}"',
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    ).communicate()
 
-logging.debug(f"stdout: {stdout}")
-logging.debug(f"stderr: {stderr}")
+    logging.debug(f"stdout: {stdout}")
+    logging.debug(f"stderr: {stderr}")
 
-if "Submitted batch job" not in str(stdout, encoding="utf-8"):
-    raise RuntimeError("Something gone wrong, job was not launched.")
+    if "Submitted batch job" not in str(stdout, encoding="utf-8"):
+        raise RuntimeError("Something gone wrong, job was not launched.")
+
+
+if __name__ == "__main__":
+    # Parsing API input, requires --query keyword, with optional --script
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", type=str, required=True)
+    parser.add_argument("--script", type=str, required=False)
+    args = parser.parse_args()
+    logging.debug(f"API input (launcher): {args}")
+    exit()
+    launch_job(query=args.query, script=args.script)
