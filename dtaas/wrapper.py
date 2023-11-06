b80fd81e747429827dc9c855e03df808e5e85d6d
Luca Babetto <l.babetto@cineca.it>
2023-11-06 10:49:15 +0100

    cleaned up some code

--- dtaas/wrapper.py
+++ dtaas/wrapper.py
@@ -1,11 +1,13 @@
 """
-Wrapper which needs to be run providing a --query flag from command line with a SQL query and (optionally) a 
---script flag with a Python script in text form. Ideally both flag arguments should be encased in triple quotes
+Wrapper which needs to be run providing a --query flag from command line with the path to a file containing an SQL query 
+and (optionally) a --script flag with the path to a Python script. This Python script should contain a `main` function
+which takes as input a list (of file paths), does some user-defined analysis, and returns a list (of file paths) which
+the wrapper then takes and stores in a compressed archive. TODO: S3 implementation and POST to datalake.
 
-The wrapper then does the following:
+Called from command line and given the query and scripts, the wrapper does the following:
 
-  1. Takes user SQL query and converts it to Mongo spec using custom sqlparse codebase (@lbabetto/sqlparse)
-  2. Runs query and retrieves the matching files, generating a list with the paths to the files
+  1. Takes user SQL query and converts it to Mongo spec using a custom sqlparse codebase (@lbabetto/sqlparse)
+  2. Runs query on remote DB and retrieves the matching entries, generating a list with the paths to the files
   3. Takes the user-provided python script and runs it locally, feeding the input files list to the `main` function
   4. Retrieves the output files from the `main` function and saves the files (of any kind, user-defined) in a zipped 
   archive which the user can then download
@@ -45,6 +47,7 @@ def convert_SQL_to_mongo(sql_query: str) -> Tuple[Dict[str, str], Dict[str, str]
     Tuple[Dict[str, str], Dict[str, str]]
         dictionaries containing the filters (WHERE) and fields (SELECT) in MongoDB spec
     """
+
     builder = MongoQueryBuilder()
     logger.info(f"User query: {sql_query}")
 
@@ -59,8 +62,10 @@ def convert_SQL_to_mongo(sql_query: str) -> Tuple[Dict[str, str], Dict[str, str]
         logger.debug("Missing WHERE clause from SQL query, returning all data in the database.")
         query_fields = "{}"
         query_filters = "{}"
+
     logger.info(f"MongoDB query filter: {query_filters}")
     logger.info(f"MongoDB query fields: {query_fields}")
+
     return query_filters, query_fields
 
 
@@ -85,10 +90,14 @@ def retrieve_files(
     List[str]
         list containing the paths of the files matching the query
     """
+
     query_matches = []
+
     for entry in collection.find(query_filters, query_fields):
         query_matches.append(entry["path"])
+
     logger.debug(f"Query results: {query_matches}")
+
     return query_matches
 
 
@@ -98,7 +107,7 @@ def run_script(script_path: str, files_in: List[str]) -> List[str]:
 
     Parameters
     ----------
-    script_file : str
+    script_path : str
         path to the Python script provided by the user, to be run on the query results
     files_in : List[str]
         list of path with the files on which to run the script
@@ -110,9 +119,14 @@ def run_script(script_path: str, files_in: List[str]) -> List[str]:
 
     Raises
     ------
+    NameError
+        if the user provides a script whose name conflicts with another loaded library
+    AttributeError
+        if the user provides a script without a `main` function
     TypeError
         if the user-provided script `main` function does not return a list, abort the run
     """
+
     logger.info(f"User script: {script_path}")
 
     module_name = os.path.splitext(script_path)[0]
@@ -160,6 +174,7 @@ def save_output(files_out: List[str]):
         shutil.copy(file, f"RESULTS/{os.path.basename(file)}")
     shutil.make_archive("results", "zip", "RESULTS")
     shutil.rmtree("RESULTS")
+
     logger.info("Processed files available in the results.zip archive")
 
 
@@ -181,21 +196,20 @@ def run_wrapper(
     script_path : str, optional
         path to the Python script provided by the user, to be run on the query results
     """
-    query_filters, query_fields = convert_SQL_to_mongo(sql_query=sql_query)
-    files_in = retrieve_files(
-        collection=collection,
-        query_fields=query_fields,
-        query_filters=query_filters,
-    )
+
+    query_filters, query_fields = convert_SQL_to_mongo(sql_query)
+
+    files_in = retrieve_files(collection, query_fields, query_filters)
+
     if script_path:
-        files_out = run_script(script_path=script_path, files_in=files_in)
-        save_output(files_out=files_out)
+        files_out = run_script(script_path, files_in)
+        save_output(files_out)
     else:
-        save_output(files_out=files_in)
+        save_output(files_in)
 
 
 if __name__ == "__main__":
-    # loading config and setting up URI
+    # loading config and setting up MongoDB URI
     config = load_config()
     mongodb_uri = f"mongodb://{config['MONGO']['user']}:{config['MONGO']['password']}@{config['MONGO']['ip']}:{config['MONGO']['port']}/"
 
@@ -211,8 +225,4 @@ if __name__ == "__main__":
     query_path, script_path = parse_cli_input()
     with open(query_path) as query_file:
         sql_query = query_file.read()
-        run_wrapper(
-            collection=collection,
-            sql_query=sql_query,
-            script_path=script_path,
-        )
+        run_wrapper(collection, sql_query, script_path)
