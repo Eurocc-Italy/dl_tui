6341dcb869449509bf558d50551603943a729975
Luca Babetto <l.babetto@cineca.it>
2023-11-14 09:36:49 +0100

    updated setup.py and inserted main function in executables

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ setup(
     package_data={
         "dtaas": ["*", "bin/*", "tuilib/*", "etc/*"],
     },
-    scripts=["dtaas/bin/dtaas_tui_client", "dtaas/bin/dtaas_tui_server"],
+    entry_points={
+        "console_scripts": [
+            "dtaas_tui_client=dtaas.bin.dtaas_tui_client:main",
+            "dtaas_tui_server=dtaas.bin.dtaas_tui_server:main",
+        ],
+    },
     install_requires=[],
     author="Luca Babetto",
     author_email="l.babetto@cineca.it",
