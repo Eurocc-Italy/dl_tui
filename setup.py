c24720d87d8f6e10aecda62703da3b31fcd8a9d2
Luca Babetto <l.babetto@cineca.it>
2023-11-14 09:29:08 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="DTaaS",
+    name="DTaaS_TUI",
     version="0.1",
     description="Query/Process interface for DTaaS",
-    packages=["dtaas"],
+    packages=find_packages(include=["dtaas"]),
     package_data={
-        "dtaas": ["*", "bin", "tuilib", "etc"],
+        "dtaas": ["*", "bin/*", "tuilib/*", "etc/*"],
     },
     scripts=["dtaas/bin/dtaas_tui_client", "dtaas/bin/dtaas_tui_server"],
     install_requires=[],
+    author="Luca Babetto",
+    author_email="l.babetto@cineca.it",
 )
