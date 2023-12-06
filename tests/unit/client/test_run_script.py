73665d55b460714fa6596845890ada72a3fc7559
Luca Babetto <l.babetto@cineca.it>
2023-12-06 11:48:32 +0100

    updated tests

--- tests/unit/client/test_run_script.py
+++ tests/unit/client/test_run_script.py
@@ -5,11 +5,20 @@ import pytest
 #
 
 from dtaas.tuilib.client import run_script
+import os
+import shutil
+from sh import pushd
 
-# TODO: run_script no longer works in a temporary directory, implement this explicitly!
+
+@pytest.fixture(scope="function")
+def create_tmpdir():
+    tmpdir = "run_script_test"
+    os.makedirs(tmpdir)
+    yield tmpdir
+    shutil.rmtree(tmpdir)
 
 
-def test_run_script(generate_test_files):
+def test_run_script(generate_test_files, create_tmpdir):
     """
     Search for two specific files and return them in reverse order
     """
@@ -18,29 +27,32 @@ def test_run_script(generate_test_files):
 
     files_in = generate_test_files
 
-    files_out = run_script(script=script, files_in=files_in)
+    with pushd(create_tmpdir):
+        files_out = run_script(script=script, files_in=files_in)
 
     assert files_out == [
-        "TESTFILE_2.txt",
-        "TESTFILE_1.txt",
+        f"{os.getcwd()}/run_script_test/TESTFILE_2.txt",
+        f"{os.getcwd()}/run_script_test/TESTFILE_1.txt",
     ], "Output file list not matching"
 
 
-def test_missing_main():
+def test_missing_main(create_tmpdir):
     """
     Test that if no `main` function is present, the program crashes
     """
     with pytest.raises(AttributeError):
         script = "answer = 42"
         files_in = []
-        run_script(script=script, files_in=files_in)
+        with pushd(create_tmpdir):
+            run_script(script=script, files_in=files_in)
 
 
-def test_main_wrong_return():
+def test_main_wrong_return(create_tmpdir):
     """
     Test that if `main` function does not return a list, the program crashes
     """
     with pytest.raises(TypeError):
         script = "def main(files_in):\n return 42"
         files_in = []
-        run_script(script=script, files_in=files_in)
+        with pushd(create_tmpdir):
+            run_script(script=script, files_in=files_in)
