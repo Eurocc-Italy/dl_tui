dc501997ac6c7fa1411d6353aa8619f941b0120a
lbabetto <l.babetto@cineca.it>
2024-05-14 09:15:49 +0200

    updated repo name

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="dlaas-tui",
-    version="0.3.9",
+    name="dl-tui",
+    version="0.4.0",
     description="Text user interface for Cineca's Data Lake as a Service",
     packages=find_packages(include=["dlaas", "dlaas.*"]),
     package_data={
