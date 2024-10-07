5812edfca595406d93451bc9010e09943f6b3aa6
lbabetto <l.babetto@cineca.it>
2024-10-07 15:36:55 +0200

    Add regex comment

--- dlaas/tuilib/common.py
+++ dlaas/tuilib/common.py
@@ -50,7 +50,7 @@ def sanitize_dictionary(dictionary: Dict[str, str]) -> None:
         ],  # any word sequence (no .) delimited by slashes, starting with /
         "omp_num_threads": [r"[0-9]+"],  # any number,
         "mpi_np": [r"[0-9]+"],  # any number,
-        "modules": [r"\[('([a-zA-Z0-9_.-]+\/?)+',? ?)+\]"],  # any word sequence delimited by slashes
+        "modules": [r"\[('([a-zA-Z0-9_.-]+\/?)+',? ?)+\]"],  # list of module names, delmited by commas
         # config_server
         "user": [r"[a-zA-Z0-9_]+"],  # any single word (word: character sequence containing alphanumerics or _)
         "host": [
